a3789feaac34f6a54254f33ba6882e2faa5c3157
student <student@imsedu.local>
2020-10-29 13:13:37 +0500

    Wrote insertion sort method

--- Week-4/SortingApp/src/sortingapp/SortingAlgos.java
+++ Week-4/SortingApp/src/sortingapp/SortingAlgos.java
@@ -61,6 +61,22 @@ public class SortingAlgos
 //        System.out.println("Number of swaps performed were:"+bSwap);
 //        System.out.println("No of comparisons performed were :"+bComp);
     }
+    void insertionSort(int arr[], int n)  
+{  
+    int i, key, j;  
+    for (i = 1; i < n; i++) 
+    {  
+        key = arr[i];  
+        j = i - 1;  
+  
+        while (j >= 0 && arr[j] > key) 
+        {  
+            arr[j + 1] = arr[j];  
+            j--;  
+        }  
+        arr[j + 1] = key;  
+    }  
+}  
     
     public void PrintList(int List[])
     {
